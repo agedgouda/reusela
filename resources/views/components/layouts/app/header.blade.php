18c8942e25a79705bef3c9a6fd2b6b79cfdd6fc5
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2026-02-24 21:32:14 -0800

    fontasy

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -5,7 +5,7 @@
         @include('partials.head')
 
         <script src="https://cdn.tiny.cloud/1/6v20tgz6hhmy6brv7zwsq3z1q36agqhkfht3e165e7ucldv5/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://use.typekit.net/bdf6fuz.css">
+        <link rel="stylesheet" href="https://use.typekit.net/ovt6npm.css">
     </head>
     <body class="min-h-screen bg-white">
         <flux:header  class="mx-0 border-b border-zinc-200 bg-zinc-50">
