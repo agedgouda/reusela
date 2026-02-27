0937988acf89c27a4b622e1f9c85e49cd536eaa2
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2026-02-26 17:41:18 -0800

    fix stuff

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -22,9 +22,6 @@ class Section extends Model
     protected static function booted()
     {
         static::saved(function ($section) {
-            \Log::info('--- SECTION SAVED START ---');
-            \Log::info('Section ID: ' . $section->id);
-            \Log::info('Linked Jurisdiction ID: ' . ($section->jurisdiction_id ?? 'NULL'));
 
             // Check the driver - if this says 'file', tags will NOT work
             $driver = config('cache.default');
